a258167359d7298225bb30d65eaa3fde886fc2e1
cmharlow <cmharlow@gmail.com>
2019-09-17 19:14:47 -0400

    fix missing class warning, script not finding HOME (bash -ux workaround)

--- scripts/index.sh
+++ scripts/index.sh
@@ -3,10 +3,10 @@
 # have any error in following cause bash script to fail
 set -e
 # export / set all environment variables passed here by task for pick-up by subprocess
-set -a
-set -ux
+set -aux
 
 # required for picking up rbenv variables; source for VMs, export for docker.
+export HOME=$AIRFLOW_USER_HOME
 source ~/.bashrc
 export PATH="$AIRFLOW_HOME/.rbenv/shims:$AIRFLOW_HOME/.rbenv/bin:$PATH"
 
@@ -14,6 +14,7 @@ export PATH="$AIRFLOW_HOME/.rbenv/shims:$AIRFLOW_HOME/.rbenv/bin:$PATH"
 git clone https://github.com/tulibraries/funnel_cake_index.git tmp/funnel_cake_index
 cd tmp/funnel_cake_index
 gem install bundler
+bundle install
 
 # grab list of items from designated aws bucket (creds are envvars), then index each item
 for record_set in `aws s3api list-objects --bucket $BUCKET --prefix $FOLDER | jq -r '.Contents[].Key'`
