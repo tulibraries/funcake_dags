9281854e5f6a41618eb2c45d2c7051a0c407930d
Steven Ng <steven@nomadicoder.com>
2019-10-24 16:10:11 -0400

    Create dev and prod dags

--- dev_combine_index_dag.py
+++ dev_combine_index_dag.py
@@ -37,10 +37,10 @@ def slackpostonsuccess(dag, **context):
 
 # Get Solr URL & Collection Name for indexing info; error out if not entered
 SOLR_CONN = BaseHook.get_connection("SOLRCLOUD")
-CONFIGSET = Variable.get("FUNCAKE_CONFIGSET")
+CONFIGSET = Variable.get("FUNCAKE_DEV_CONFIGSET")
 TIMESTAMP = "{{ execution_date.strftime('%Y-%m-%d_%H-%M-%S') }}"
 COLLECTION = CONFIGSET + "-" + TIMESTAMP
-ALIAS = CONFIGSET
+ALIAS = Variable.get("FUNCAKE_DEV_ALIAS")
 if "://" in SOLR_CONN.host:
     SOLR_COLL_ENDPT = SOLR_CONN.host + ":" + str(SOLR_CONN.port) + "/solr/" + COLLECTION
 else:
@@ -72,7 +72,7 @@ DEFAULT_ARGS = {
 }
 
 FCDAG = DAG(
-    'funcake_index',
+    'dev_funcake_index',
     default_args=DEFAULT_ARGS,
     catchup=False,
     max_active_runs=1,
