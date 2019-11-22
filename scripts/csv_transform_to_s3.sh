f636d6928da5b1f4b33719752013012e590d8b3d
sensei100 <tuh42082@temple.edu>
2019-11-22 16:19:48 -0500

    remove source line

--- scripts/csv_transform_to_s3.sh
+++ scripts/csv_transform_to_s3.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-source ~/.bashrc
 git clone https://tulibraries-devops:$GITHUB_TOKEN@github.com/tulibraries/aggregator_data.git
 cd aggregator_data/$DAGID
 cp $HOME/dags/funcake_dags/scripts/csv_to_xml.py .
