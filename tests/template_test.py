557d062b7e64e24a068e66ea275f9ab034e60b0e
David Kinzer <dtkinzer@gmail.com>
2020-03-11 10:39:04 -0400

    Initial commit for funcake_dags.template.create_dag feature.

---
+++ tests/template_test.py
@@ -0,0 +1,95 @@
+import airflow
+from funcake_dags.template import create_dag
+import unittest
+from datetime import datetime, timedelta
+
+AIRFLOW_APP_HOME = airflow.models.Variable.get("AIRFLOW_HOME")
+
+SCRIPTS_PATH = AIRFLOW_APP_HOME + "/dags/funcake_dags/scripts"
+
+class TestTemplate(unittest.TestCase):
+    def setUp(self):
+        airflow.models.Variable.set("TEST_OAI_CONFIG", {
+            "xsl_branch": "master",
+            "xsl_filename": "transforms/dplah.xsl",
+            "xsl_repo": "tulibraries/aggregator_mdx",
+            "schematron_filter": "validations/padigital_reqd_fields.sch",
+            "schematron_report": "validations/padigital_missing_thumbnailURL.sch",
+            "endpoint": "foobar",
+            }, serialize_json=True)
+
+        airflow.models.Variable.set("TEST_XSL_CONFIG", {
+            "schematron_filter": "validations/funcake_reqd_fields.sch",
+            "schematron_report": "validations/padigital_missing_thumbnailURL.sch",
+            "xsl_branch": "master",
+            "xsl_filename": "transforms/dplah.xsl",
+            "xsl_repository": "tulibraries/aggregator_mdx",
+            }, serialize_json=True)
+
+        self.dag = create_dag(
+                dag_id="foo",
+                oai_config_name="TEST_OAI_CONFIG",
+                xsl_config_name="TEST_XSL_CONFIG",
+                target_alias_env_name="TEST_ALIAS_ENV")
+
+    def test_create_dag(self):
+        """Assert expected DAG exists."""
+        self.assertEqual(self.dag.dag_id, "foo")
+
+    def test_set_collection_name_task(self):
+        task = self.dag.get_task("set_collection_name")
+        self.assertIn(task.python_callable.__qualname__, "datetime.strftime")
+
+    def test_harvest_oai_task(self):
+        task = self.dag.get_task("harvest_oai")
+        self.assertEqual(task.op_kwargs["bucket_name"], "test-s3-bucket")
+        self.assertEqual(task.op_kwargs["oai_endpoint"], "foobar")
+
+    def test_harvest_schematron_report_task(self):
+        task = self.dag.get_task("harvest_schematron_report")
+        self.assertEqual(task.op_kwargs["bucket"], "test-s3-bucket")
+        self.assertEqual(task.op_kwargs["destination_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/new-updated")
+        self.assertEqual(task.op_kwargs["source_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/new-updated/")
+
+    def test_harves_filter_task(self):
+        task = self.dag.get_task("harvest_filter")
+        self.assertEqual(task.op_kwargs["bucket"], "test-s3-bucket")
+        self.assertEqual(task.op_kwargs["schematron_filename"], "validations/padigital_reqd_fields.sch")
+        self.assertEqual(task.op_kwargs["destination_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/new-updated-filtered/")
+        self.assertEqual(task.op_kwargs["report_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/harvest_filter")
+        self.assertEqual(task.op_kwargs["source_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/new-updated/")
+
+    def test_xsl_transform_task(self):
+        task = self.dag.get_task("xsl_transform")
+        self.assertEqual(task.bash_command, SCRIPTS_PATH + "/transform.sh " )
+        self.assertEqual(task.env["XSL_BRANCH"], "master" )
+
+    def test_xsl_transform_schematron_report_task(self):
+        task = self.dag.get_task("xsl_transform_schematron_report")
+        self.assertEqual(task.op_kwargs["schematron_filename"], "validations/padigital_missing_thumbnailURL.sch")
+        self.assertEqual(task.op_kwargs["destination_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/transformed")
+        self.assertEqual(task.op_kwargs["source_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/transformed/")
+
+    def test_xsl_transform_filter_task(self):
+        task = self.dag.get_task("xsl_transform_filter")
+        self.assertEqual(task.op_kwargs["schematron_filename"], "validations/funcake_reqd_fields.sch")
+        self.assertEqual(task.op_kwargs["destination_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/transformed-filtered/")
+        self.assertEqual(task.op_kwargs["source_prefix"], "foo/{{ ti.xcom_pull(task_ids='set_collection_name') }}/transformed/")
+
+    def test_refresh_alias_task(self):
+        task = self.dag.get_task("refresh_sc_collection_for_alias")
+        self.assertEqual(task.op_kwargs["alias"], "funcake-oai-0-dev")
+        self.assertEqual(task.op_kwargs["collection"], "funcake-oai-0-foo-dev")
+
+    def test_pulish_stask(self):
+        task = self.dag.get_task("publish")
+        self.assertEqual(task.bash_command, SCRIPTS_PATH + "/index.sh " )
+        self.assertEqual(task.env["FUNCAKE_OAI_SOLR_URL"], "http://127.0.0.1:8983/solr/funcake-oai-0-foo-dev" )
+
+    def test_success_slack_trigger__task(self):
+        task = self.dag.get_task("success_slack_trigger")
+        self.assertIn(task.python_callable.__qualname__, "slackpostonsuccess")
+
+    def test_all_task_are_linked_to_something(self):
+        for task in self.dag.tasks:
+            self.assertTrue(task.upstream_list != [] or task.downstream_list != [], "Expect all tasks to be linked to eachother.")
