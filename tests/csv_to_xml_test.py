b3e0ffac1eb72264db7e0b274533e00e53bfda3f
David Kinzer <dtkinzer@gmail.com>
2021-06-16 11:15:45 -0400

    Revert "Enforce utf-8 string in handling xml files."

--- tests/csv_to_xml_test.py
+++ tests/csv_to_xml_test.py
@@ -9,7 +9,7 @@ class TestCsvToXml(unittest.TestCase):
         csv_string = "foo, bar, buzz\n1,,2"
         csv_reader = csv.reader(csv_string.splitlines(), delimiter=',')
 
-        expected = '<collection dag-id="foo" dag-timestamp="bar">\n  <record airflow-record-id="1">\n    <foo>1</foo>\n    <buzz>2</buzz>\n  </record>\n</collection>\n'
+        expected = b'<collection dag-id="foo" dag-timestamp="bar">\n  <record airflow-record-id="1">\n    <foo>1</foo>\n    <buzz>2</buzz>\n  </record>\n</collection>\n'
         actual = csv_reader_to_xml_string(csv_reader, "foo", "bar", counter)
         self.assertEqual(actual, expected, "Assert that empty fields are not added.")
         self.assertEqual(counter.count, 1, "Assert that this method updates the counter.")
