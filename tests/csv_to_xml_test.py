c51c9b8c05872ae1e6608dd7b2372184d8f50310
David Kinzer <dtkinzer@gmail.com>
2021-08-24 11:36:39 -0400

    FLYPIE-285: Add utf-8 encoding to csv to xml transform.
    Fixes FLYPIE-285

--- tests/csv_to_xml_test.py
+++ tests/csv_to_xml_test.py
@@ -1,6 +1,9 @@
 import unittest
 from funcake_dags.scripts.csv_to_xml import csv_reader_to_xml_string, Counter
 import csv
+import shutil
+import tempfile
+import os
 
 class TestCsvToXml(unittest.TestCase):
 
@@ -13,3 +16,18 @@ class TestCsvToXml(unittest.TestCase):
         actual = csv_reader_to_xml_string(csv_reader, "foo", "bar", counter)
         self.assertEqual(actual, expected, "Assert that empty fields are not added.")
         self.assertEqual(counter.count, 1, "Assert that this method updates the counter.")
+
+
+    def test_csv_to_xml(self):
+        orig_cwd = os.getcwd()
+        tmp_dir = tempfile.TemporaryDirectory(dir="tests/tmp")
+
+        shutil.copy2("tests/fixtures/csv-to-xml.csv", tmp_dir.name + "/csv-to-xml.csv")
+        shutil.copy2("funcake_dags/scripts/csv_to_xml.py", tmp_dir.name + "/csv_to_xml.py")
+        os.chdir(tmp_dir.name)
+        output = os.system("DAGID=foo TIMESTAMP=bar python csv_to_xml.py")
+
+        # If this fails you need to debug it using pdb.set_trace()
+        self.assertEqual(output, 0, "Script execution did not fail.")
+        self.assertIn("“Sarijs manuscripts,”", open("csv-to-xml.xml").read())
+        os.chdir(orig_cwd)
