d6f1a9e6860673817c0e3c500ccd7bbfffc22dd5
sensei100 <tuh42082@temple.edu>
2022-05-26 14:55:41 -0400

    Update conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import airflow
 import json
+from airflow.models import Variable, Connection
+from airflow.settings import Session
 
 def pytest_sessionstart():
     """
@@ -11,29 +13,29 @@ def pytest_sessionstart():
     file after command line options have been parsed.
     """
     repo_dir = os.getcwd()
-    subprocess.run("airflow initdb", shell=True)
+    subprocess.run("airflow db init", shell=True)
     subprocess.run("mkdir -p dags/funcake_dags", shell=True)
     subprocess.run("mkdir -p data", shell=True)
     subprocess.run("mkdir -p logs", shell=True)
     subprocess.run("cp *.py dags/funcake_dags", shell=True)
     subprocess.run("cp -r scripts dags/funcake_dags", shell=True)
-    airflow.models.Variable.set("AIRFLOW_HOME", repo_dir)
-    airflow.models.Variable.set("AIRFLOW_USER_HOME", repo_dir)
-    airflow.models.Variable.set("AIRFLOW_DATA_BUCKET", "test-s3-bucket")
-    airflow.models.Variable.set("AIRFLOW_LOG_DIR", repo_dir + '/logs')
-    airflow.models.Variable.set("FREE_LIBRARY_CSV_SCHEMATRON_FILTER", "validations/dcingest_reqd_fields.sch")
-    airflow.models.Variable.set("FREE_LIBRARY_CSV_SCHEMATRON_REPORT", "validations/padigital_missing_thumbnailURL.sch")
-    airflow.models.Variable.set("FREE_LIBRARY_XSL_CONFIG", {"xsl_branch": "main", "xsl_filename": "transforms/dplah.xsl", "xsl_repo": "tulibraries/aggregator_mdx", "schematron_filter": "validations/padigital_reqd_fields.sch", "schematron_report": "validations/padigital_missing_thumbnailURL.sch"}, serialize_json=True)
-    airflow.models.Variable.set("FUNCAKE_DEV_CONFIGSET", "funcake-0")
-    airflow.models.Variable.set("FUNCAKE_PROD_CONFIGSET", "funcake-0")
-    airflow.models.Variable.set("FUNCAKE_OAI_CONFIG", {"endpoint": "http://localhost/oai", "include_sets": ["i_love_cats"], "exclude_sets": [], "md_prefix": "kittens"}, serialize_json=True)
-    airflow.models.Variable.set("FUNCAKE_SOLR_CONFIG", {"configset": "funcake-0", "replication_factor": 4}, serialize_json=True)
-    airflow.models.Variable.set("VILLANOVA_OAI_CONFIG", {"endpoint": "http://localhost/oai", "included_sets": ["i_love_cats"], "excluded_sets": [], "md_prefix": "kittens"}, serialize_json=True)
-    airflow.models.Variable.set("VILLANOVA_XSL_CONFIG", {"xsl_branch": "main", "xsl_filename": "transforms/villanova.xsl", "xsl_repo": "tulibraries/aggregator_mdx", "schematron_filter": "validations/padigital_reqd_fields.sch", "schematron_report": "validations/padigital_missing_thumbnailURL.sch"}, serialize_json=True)
-    airflow.models.Variable.set("VILLANOVA_SOLR_CONFIGSET", "funcake-oai-0")
-    airflow.models.Variable.set("VILLANOVA_TARGET_ALIAS_ENV", "qa")
+    Variable.set("AIRFLOW_HOME", repo_dir)
+    Variable.set("AIRFLOW_USER_HOME", repo_dir)
+    Variable.set("AIRFLOW_DATA_BUCKET", "test-s3-bucket")
+    Variable.set("AIRFLOW_LOG_DIR", repo_dir + '/logs')
+    Variable.set("FREE_LIBRARY_CSV_SCHEMATRON_FILTER", "validations/dcingest_reqd_fields.sch")
+    Variable.set("FREE_LIBRARY_CSV_SCHEMATRON_REPORT", "validations/padigital_missing_thumbnailURL.sch")
+    Variable.set("FREE_LIBRARY_XSL_CONFIG", {"xsl_branch": "main", "xsl_filename": "transforms/dplah.xsl", "xsl_repo": "tulibraries/aggregator_mdx", "schematron_filter": "validations/padigital_reqd_fields.sch", "schematron_report": "validations/padigital_missing_thumbnailURL.sch"}, serialize_json=True)
+    Variable.set("FUNCAKE_DEV_CONFIGSET", "funcake-0")
+    Variable.set("FUNCAKE_PROD_CONFIGSET", "funcake-0")
+    Variable.set("FUNCAKE_OAI_CONFIG", {"endpoint": "http://localhost/oai", "include_sets": ["i_love_cats"], "exclude_sets": [], "md_prefix": "kittens"}, serialize_json=True)
+    Variable.set("FUNCAKE_SOLR_CONFIG", {"configset": "funcake-0", "replication_factor": 4}, serialize_json=True)
+    Variable.set("VILLANOVA_OAI_CONFIG", {"endpoint": "http://localhost/oai", "included_sets": ["i_love_cats"], "excluded_sets": [], "md_prefix": "kittens"}, serialize_json=True)
+    Variable.set("VILLANOVA_XSL_CONFIG", {"xsl_branch": "main", "xsl_filename": "transforms/villanova.xsl", "xsl_repo": "tulibraries/aggregator_mdx", "schematron_filter": "validations/padigital_reqd_fields.sch", "schematron_report": "validations/padigital_missing_thumbnailURL.sch"}, serialize_json=True)
+    Variable.set("VILLANOVA_SOLR_CONFIGSET", "funcake-oai-0")
+    Variable.set("VILLANOVA_TARGET_ALIAS_ENV", "qa")
 
-    solrcloud = airflow.models.Connection(
+    solrcloud = Connection(
         conn_id="SOLRCLOUD",
         conn_type="http",
         host="127.0.0.1",
@@ -41,7 +43,7 @@ def pytest_sessionstart():
         login="puppy",
         password="chow"
     )
-    solrcloud_writer = airflow.models.Connection(
+    solrcloud_writer = Connection(
         conn_id="SOLRCLOUD-WRITER",
         conn_type="http",
         host="127.0.0.1",
@@ -49,19 +51,19 @@ def pytest_sessionstart():
         login="puppy",
         password="chow"
     )
-    s3 = airflow.models.Connection(
+    s3 = Connection(
         conn_id="AIRFLOW_S3",
         conn_type="AWS",
         login="elephants-key",
         password="elephants-secret"
     )
-    slack = airflow.models.Connection(
+    slack = Connection(
         conn_id="AIRFLOW_CONN_SLACK_WEBHOOK",
         conn_type="http",
         host="127.0.0.1/services",
         port="",
     )
-    airflow_session = airflow.settings.Session()
+    airflow_session = Session()
     airflow_session.add(solrcloud)
     airflow_session.add(solrcloud_writer)
     airflow_session.add(s3)
@@ -76,4 +78,4 @@ def pytest_sessionfinish():
     subprocess.run("rm -rf dags", shell=True)
     subprocess.run("rm -rf data", shell=True)
     subprocess.run("rm -rf logs", shell=True)
-    subprocess.run("yes | airflow resetdb", shell=True)
+    subprocess.run("yes | airflow db reset", shell=True)
